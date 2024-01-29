6d761aa16c01f075c90e4f85a388f7a86e479f66
incodeEfren <incode.devs@cpcoriente.org>
2024-01-29 16:06:05 -0500

    Dashboard 2

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -7,11 +7,13 @@ const Dashboard = () => {
   return (
     <div className="flex h-screen overflow-hidden">
       <DashNavigation />
-      <div className="flex-1 bg-gray-800 text-white p-2">
+      <div className="flex-1 bg-gray-800 text-white p-2 overflow-y-scroll md:overflow-hidden">
         {/* ESTADÍSTICAS GENERALES */}
         <div className="p-2">
-          <h1 className="text-4xl flex gap-1"> <FaChartPie/> Estadísticas</h1>
-          <div className="flex gap-3 mt-2">
+          <h1 className="text-4xl flex gap-1">
+            <FaChartPie className="text-3xl mt-2"/> Estadísticas
+          </h1>
+          <div className="flex flex-col md:flex-row gap-3 mt-2">
             <div className="flex-1">
               <div className="w-full h-52 bg-primary text-center rounded">
                 <p className="text-lg">Personas</p>
@@ -42,10 +44,222 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-        {/*  */}
-        <div className="p-2">
-
+        
+        {/* SOLICITUDES */}
+        <div className="p-2 flex flex-col md:flex-row">
+          {/* SOLICITUDES OFERTANTES */}
+          <div className="flex-1">
+            <h1>Solicitudes de Ofertantes</h1>
+            <div className="p-4">
+              <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3">
+                        Solicitante
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Empresa
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Fecha Solicitud
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Estado
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Acciones
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Cosme Fulanito Barajas
+                      </th>
+                      <td className="px-6 py-4">
+                        Cosme S.A.S
+                      </td>
+                      <td className="px-6 py-4">
+                        2024-02-25
+                      </td>
+                      <td className="px-6 py-4">
+                        Solicitado
+                      </td>
+                      <td className="px-6 py-4">
+                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                      </td>
+                    </tr>
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Cosme Fulanito Barajas
+                      </th>
+                      <td className="px-6 py-4">
+                        Cosme S.A.S
+                      </td>
+                      <td className="px-6 py-4">
+                        2024-02-25
+                      </td>
+                      <td className="px-6 py-4">
+                        Solicitado
+                      </td>
+                      <td className="px-6 py-4">
+                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                      </td>
+                    </tr>
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Cosme Fulanito Barajas
+                      </th>
+                      <td className="px-6 py-4">
+                        Cosme S.A.S
+                      </td>
+                      <td className="px-6 py-4">
+                        2024-02-25
+                      </td>
+                      <td className="px-6 py-4">
+                        Solicitado
+                      </td>
+                      <td className="px-6 py-4">
+                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                      </td>
+                    </tr>
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Cosme Fulanito Barajas
+                      </th>
+                      <td className="px-6 py-4">
+                        Cosme S.A.S
+                      </td>
+                      <td className="px-6 py-4">
+                        2024-02-25
+                      </td>
+                      <td className="px-6 py-4">
+                        Solicitado
+                      </td>
+                      <td className="px-6 py-4">
+                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                      </td>
+                    </tr>
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Cosme Fulanito Barajas
+                      </th>
+                      <td className="px-6 py-4">
+                        Cosme S.A.S
+                      </td>
+                      <td className="px-6 py-4">
+                        2024-02-25
+                      </td>
+                      <td className="px-6 py-4">
+                        Solicitado
+                      </td>
+                      <td className="px-6 py-4">
+                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                      </td>
+                    </tr>
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          {/* SOLICITUDES RETOS */}
+          <div className="flex-1">
+            <h1>Solicitudes de Retos</h1>
+            <div className="p-2 h-48">
+              <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3">
+                        Solicitante
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Empresa
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Fecha Solicitud
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Estado
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Acciones
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Cosme Fulanito Barajas
+                      </th>
+                      <td className="px-6 py-4">
+                        Cosme S.A.S
+                      </td>
+                      <td className="px-6 py-4">
+                        2024-02-25
+                      </td>
+                      <td className="px-6 py-4">
+                        Solicitado
+                      </td>
+                      <td className="px-6 py-4">
+                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <h1>PQRS</h1>
+            <div className="p-2">
+              <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table className="w-full text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                      <th scope="col" className="px-6 py-3">
+                        Solicitante
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Empresa
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Fecha Solicitud
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Estado
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                        Acciones
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Cosme Fulanito Barajas
+                      </th>
+                      <td className="px-6 py-4">
+                        Cosme S.A.S
+                      </td>
+                      <td className="px-6 py-4">
+                        2024-02-25
+                      </td>
+                      <td className="px-6 py-4">
+                        Solicitado
+                      </td>
+                      <td className="px-6 py-4">
+                        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                      </td>
+                    </tr>
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
+
       </div>
     </div>
   )
