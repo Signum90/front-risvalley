730a2320e0e7a7d605a8d70a274afb50f50d3209
incodeEfren <incode.devs@cpcoriente.org>
2024-02-01 15:06:07 -0500

    Dash directory 1

--- src/pages/dashboard/directory/DashDirectory.jsx
+++ src/pages/dashboard/directory/DashDirectory.jsx
@@ -6,14 +6,42 @@ const DashDirectory = () => {
   return (
     <div className="fade-in">
       {/* TITULO > */}
-      <div className="p-2 border-b">
+      <div className="p-2 border-b flex gap-2">
         <h1 className="text-xl flex gap-1 font-k font-bold items-center">
           <IoIosMegaphone className="text-xl mr-1"/>Gestión de Ofertas
         </h1>
+        <div className="p-2">
+          <button className="text-sm px-2 py-1 border rounded transition-all hover:bg-black">+ Nueva Oferta</button>
+        </div>
       </div>
       {/* TABLA DE LISTADOS */}
-      <div className="p-4 mt-2">
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <div className="p-4">
+        <div className="flex gap-4 items-center">
+          <div className="flex-1">
+            <p>Solicitud de Empresas</p>
+          </div>
+          <div className="flex-1">
+            <div className="flex">
+              <div className="relative flex items-center group border-b-[3px] border-transparent hover:border-secondary cursor-pointer">
+                <button className="py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600">Filtrar</button>
+                <div className="z-50 bg-black absolute hidden group-hover:flex top-full -translate-x-[50%] left-1/2 right-1/2 w-[210px] border-b-2 border-secondary transition-colors duration-[2000] flex-col items-center">
+                  <p className="p-2 hover:text-secondary">Preguntas Frecuentes</p>
+                </div>
+              </div>
+              <div className="relative w-full">
+                  <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required/>
+                  <button type="submit" className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                    <span className="sr-only">Search</span>
+                  </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* TABLA */}
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-2">
           <table className="w-full text-xs text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
